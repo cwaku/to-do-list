3246a464b95aa6ba8b27fa125bd45c281c99a3ce
cwaku <rickymormor1@gmail.com>
2021-10-28 11:17:22 +0000

    fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import _ from "lodash";
-import "./style.css";
+import './style.css';
 
-const list = document.querySelector(".list");
+const list = document.querySelector('.list');
 
-let toDoList = [
+const toDoList = [
   2,
   {
     index: 0,
-    description: "wash the dishes",
+    description: 'wash the dishes',
     completed: false,
   },
 ];
 
 function showList() {
-  const heading = document.createElement("li");
-  heading.innerHTML = `<p>Today's To Do</p> <i class="fas fa-sync-alt icons"></i>`;
+  const heading = document.createElement('li');
+  heading.innerHTML = '<p>Today\'s To Do</p> <i class="fas fa-sync-alt icons"></i>';
   list.append(heading);
 
-  const input = document.createElement("li");
-  input.innerHTML = `<input class="text" type="text" placeholder="Add to your list..."> <i class="fas fa-stream icons"></i>`;
+  const input = document.createElement('li');
+  input.innerHTML = '<input class="text" type="text" placeholder="Add to your list..."> <i class="fas fa-stream icons"></i>';
   list.append(input);
 
   for (let i = 0; i < toDoList[0]; i += 1) {
@@ -27,11 +26,11 @@ function showList() {
     list.innerHTML += listCode;
   }
 
-  const btn = document.createElement("li");
-  btn.innerHTML = `<button type="button">Clear all completed</button>`;
+  const btn = document.createElement('li');
+  btn.innerHTML = '<button type="button">Clear all completed</button>';
   list.append(btn);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   showList();
 });
