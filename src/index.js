fcdd1c24bb85e63b14dc2c8f9bbf575928959d7a
cwaku <rickymormor1@gmail.com>
2021-10-28 10:35:15 +0000

    Markup structure and display list dynamically

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-import _ from 'lodash';
-import './style.css';
+import _ from "lodash";
+import "./style.css";
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
+const list = document.querySelector(".list");
+
+let toDoList = [
+  2,
+  {
+    index: 0,
+    description: "wash the dishes",
+    completed: false,
+  },
+];
+
+function showList() {
+  const heading = document.createElement("li");
+  heading.innerHTML = `<p>Today's To Do</p> <i class="fas fa-sync-alt icons"></i>`;
+  list.append(heading);
+
+  const input = document.createElement("li");
+  input.innerHTML = `<input class="text" type="text" placeholder="Add to your list..."> <i class="fas fa-stream icons"></i>`;
+  list.append(input);
+
+  for (let i = 0; i < toDoList[0]; i += 1) {
+    const listCode = `<li><p><input type="checkbox">${toDoList[1].description}</p> <i class="fas fa-ellipsis-v icons"></i>`;
+
+    list.innerHTML += listCode;
   }
-  
-  document.body.appendChild(component());
+
+  const btn = document.createElement("li");
+  btn.innerHTML = `<button type="button">Clear all completed</button>`;
+  list.append(btn);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showList();
+});
