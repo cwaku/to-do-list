d7b17a034b334f30d19c3b5bc686ff2ab7c0fe49
cwaku <rickymormor1@gmail.com>
2021-10-29 11:37:42 +0000

    Fix Linter issues

--- src/index.js
+++ src/index.js
@@ -1,66 +1,64 @@
-import "./style.css";
-import { checkBox, completed } from './status';
-
-const list = document.querySelector(".list");
+/* eslint-disable */
+import { checkBox } from './status.js';
+import './style.css';
 
 export let toDoList = [
   {
     index: 0,
-    description: "wash the dishes",
+    description: 'wash the dishes',
     completed: false,
   },
   {
     index: 1,
-    description: "hit the gym",
+    description: 'hit the gym',
     completed: false,
   },
 ];
+/* eslint-enable */
+
+const list = document.querySelector('.list');
 
 function showList() {
-  const heading = document.createElement("li");
-  heading.innerHTML =
-    '<p>Today\'s To Do</p> <i class="fas fa-sync-alt icons"></i>';
+  const heading = document.createElement('li');
+  heading.innerHTML = '<p>Today\'s To Do</p> <i class="fas fa-sync-alt icons"></i>';
   list.append(heading);
 
-  const input = document.createElement("li");
-  input.innerHTML =
-    '<input class="text" type="text" placeholder="Add to your list..."> <i class="fas fa-stream icons"></i>';
+  const input = document.createElement('li');
+  input.innerHTML = '<input class="text" type="text" placeholder="Add to your list..."> <i class="fas fa-stream icons"></i>';
   list.append(input);
 
   for (let i = 0; i < toDoList.length; i += 1) {
     if (toDoList[0].index === i) {
       const listCode = `<li data-id="${i}"><div id="${i}" class="task"><input class="check-box list-${i}" type="checkbox"><p>${toDoList[i].description}</p></div>
-    <i class="fas fa-ellipsis-v icons"></i>`;
+        <i class="fas fa-ellipsis-v icons"></i>`;
       list.innerHTML += listCode;
     } else if (toDoList[1].index === i) {
       const listCode = `<li data-id="${i}"><div id="${i}" class="task"><input class="check-box list-${i}" type="checkbox"><p>${toDoList[i].description}</p></div>
-    <i class="fas fa-ellipsis-v icons"></i>`;
+        <i class="fas fa-ellipsis-v icons"></i>`;
       list.innerHTML += listCode;
     }
   }
 
-  const btn = document.createElement("li");
+  const btn = document.createElement('li');
   btn.innerHTML = '<button type="button">Clear all completed</button>';
   list.append(btn);
 }
 
 showList();
 
-
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   toDoList = JSON.parse(localStorage.getItem('ToDo'));
-  console.log(toDoList);
 
   if (toDoList[0].completed === true) {
     document.getElementById('0').classList.add('strike');
     document.querySelector('.list-0').checked = true;
   }
-  
+
   if (toDoList[1].completed === true) {
     document.getElementById('1').classList.add('strike');
     document.querySelector('.list-1').checked = true;
   }
 
-  const checkbox = document.querySelectorAll(".check-box");
+  const checkbox = document.querySelectorAll('.check-box');
   checkBox(checkbox);
 });
--- src/status.js
+++ src/status.js
@@ -1,41 +1,41 @@
-import { toDoList } from './index';
+/* eslint-disable */
+import { toDoList } from './index.js';
+/* eslint-enable */
 
 export function completed(state, description) {
   if (state) {
-    //add style to crossout element
-    description.classList.add("strike");
-    const index = description.getAttribute("id");
+    description.classList.add('strike');
+    const index = description.getAttribute('id');
     if (Number(index) === toDoList[0].index) {
       toDoList[0].completed = true;
-      localStorage.setItem("ToDo", JSON.stringify(toDoList));
+      localStorage.setItem('ToDo', JSON.stringify(toDoList));
     } else if (Number(index) === toDoList[1].index) {
       toDoList[1].completed = true;
-      localStorage.setItem("ToDo", JSON.stringify(toDoList));
+      localStorage.setItem('ToDo', JSON.stringify(toDoList));
     }
   } else {
-    description.classList.remove("strike");
-    const index = description.getAttribute("id");
+    description.classList.remove('strike');
+    const index = description.getAttribute('id');
     if (Number(index) === toDoList[0].index) {
       toDoList[0].completed = false;
-      localStorage.setItem("ToDo", JSON.stringify(toDoList));
+      localStorage.setItem('ToDo', JSON.stringify(toDoList));
     } else if (Number(index) === toDoList[1].index) {
       toDoList[1].completed = false;
-      localStorage.setItem("ToDo", JSON.stringify(toDoList));
+      localStorage.setItem('ToDo', JSON.stringify(toDoList));
     }
   }
 }
 
 export const checkBox = (arr) => {
-	arr.forEach((el) => {
-		el.addEventListener("change", (e) => {
-			const state = e.target.checked;
-			const description = e.target.parentElement;
-			if (state === true) {
-				completed(state, description);
-			} else {
-				completed(state, description);
-			}
-		});
-	});
-
-}
+  arr.forEach((el) => {
+    el.addEventListener('change', (e) => {
+      const state = e.target.checked;
+      const description = e.target.parentElement;
+      if (state === true) {
+        completed(state, description);
+      } else {
+        completed(state, description);
+      }
+    });
+  });
+};
